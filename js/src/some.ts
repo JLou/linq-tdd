0f4010b2f6aa83a5085adf428b4cf80582e3f0e4
Jean-Lou PIERME <jeanlou.pierme@axa.fr>
2018-11-23 17:05:47 +0100

    ajout des solutions js

--- js/src/some.ts
+++ js/src/some.ts
@@ -1,4 +1,10 @@
 function some<T>(array: T[], predicate: (T) => boolean): boolean {
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    if (predicate(element)) {
+      return true;
+    }
+  }
   return false;
 }
 
