2a9ce2464490e423c68c4b0ec37cc9ad98d2b1f8
JLou <jlou.xd@gmail.com>
2018-11-29 21:02:51 +0100

    ajout de la solution du forEach

--- js/src/foreach.ts
+++ js/src/foreach.ts
@@ -1,6 +1,11 @@
 function forEach<T>(
   array: T[],
   callbackfn: (value: T, index: number) => void
-): void {}
+): void {
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    callbackfn(element, index);
+  }
+}
 
 export { forEach };
