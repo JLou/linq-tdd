11cf300e10d950f5cd32cff31d4bf17a2e729609
Croquet Mickael <mickael.croquet@laposte.net>
2022-09-03 20:46:51 +0200

    build: removing jest for vitest

--- js/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  roots: ["<rootDir>/src"],
-  transform: {
-    "^.+\\.tsx?$": "@swc-node/jest"
-  },
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
-};
